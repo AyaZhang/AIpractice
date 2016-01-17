146c360b9647ab937f744403f3323e9c567a11fa
Xinyi Ma <xinyima9@gmail.com>
2016-01-16 17:19:54 -0800

    DFS: maybe a little complicated

---
+++ hw2/problem21_2.py
@@ -0,0 +1,102 @@
+"""
+author:
+Xinyi Ma xim002@ucsd.edu
+"""
+
+import sys
+import fileinput
+#Problem21_1
+for line in fileinput.input():
+
+    try:
+        arrangement = [int(x.strip()) for x in line.split(',')]
+        print arrangement
+
+    except:
+        sys.exit('invalid input')
+
+    # more than 3 numbers in the same line
+    if len(arrangement) > 3:
+        sys.exit('invalid input')
+
+    # initial state not valid
+    for k in range(0, 3):
+        if arrangement[k] not in [0, 1]:
+            sys.exit('invalid input')
+
+    # determine whether the state is a goal state
+    if arrangement[0] is 0 and arrangement[1] is 0:
+        print('True')
+        sys.exit()
+    else:
+        print('False')
+
+#Problem21_2
+    frontier=[arrangement]
+    visited=[arrangement]
+    path={}
+    path[tuple(arrangement)]=''
+    action='SRL'
+    while len(frontier)!=0:
+        #print len(frontier)
+        node=frontier.pop()
+        #print 'This is node: '
+        #print node
+        #print 'path: '+path[tuple(node)]
+        room=node[2]
+        #find goal state
+        if node[0]==0 and node[1]==0:
+            print 'final path: '+path[tuple(node)]
+            sys.exit()
+        else:
+            room=node[2]
+            step=path[tuple(node)]
+            for act in action:
+                #print act
+                if act=='S':
+                    temp=list(node)
+                    temp[room]=0
+                    if temp in visited:
+                        continue
+                    else:
+                        frontier.append(temp)
+                        visited.append(temp)
+                        path[tuple(temp)]=step+act
+                    #print frontier
+                if act=='R':
+                    temp=list(node)
+                    if room==1:
+                        continue
+                    else:
+                        temp[2]=1
+                    if temp in visited:
+                        continue
+                    else:
+                        frontier.append(temp)
+                        visited.append(temp)
+                        path[tuple(temp)]=step+act
+                    #print frontier
+                if act=='L':
+                    temp=list(node)
+                    if room==0:
+                        continue
+                    else:
+                        temp[2]=0
+                        if temp in visited:
+                            continue
+                        else:
+                            frontier.append(temp)
+                            visited.append(temp)
+                            step=path[tuple(node)]
+                            path[tuple(temp)]=step+act
+                    #print frontier
+
+
+
+
+
+
+
+
+
+
