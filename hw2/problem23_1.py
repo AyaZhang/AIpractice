bff3e81082e68ac8a20bd0373b7037f44942238c
Xinyi Ma <xinyima9@gmail.com>
2016-01-18 22:04:58 -0800

    new file

--- hw2/problem23_1.py
+++ hw2/problem23_1.py
@@ -11,37 +11,31 @@ from collections import deque
 
 lineNum = 0
 input = sys.stdin.read()
-lines = input.splitlines()
-print lines
-row = len(lines)
-column = len(lines[0].split(','))
-
+text = input.splitlines()
+print text
+row = len(text)
+column = len(text[0].split(','))
+lines = []
+for j in range(0,row):
+    li = [int(x.strip()) for x in text[j].split(',')]
+    lines.append(li)
 
 #vacumm location not valid
-if len(lines[row-1]) == 3:
-    if (int(lines[row-1][0]) not in range(0,row-1)) or (int(lines[row-1][2]) not in range(0,column)):
+if len(lines[row-1]) == 2:
+    if (int(lines[row-1][0]) not in range(0,row-1)) or (int(lines[row-1][1]) not in range(0,column)):
         sys.exit('invalid input: last line')
 else:
     sys.exit('invalid input: last line')
 
 for i in range(0,row-1):
-    line=lines[i]
-    try:
-        arrangement = [int(x.strip()) for x in line.split(',')]
-        print arrangement
-        length = len(arrangement)
-
-    except:
-        sys.exit('invalid input')
-        
     # Initial state not valid
-    for k in range(0, length-1):
-        if arrangement[k] not in [0,1]:
+    for k in range(0, column):
+        if lines[i][k] not in [0,1]:
             sys.exit('invalid input')
         
     # Whether the state is a goal state
-    for k in range(0, length-1):
-        if arrangement[k] is 0:
+    for k in range(0, column):
+        if lines[i][k] is 0:
             continue
         else:
             sys.exit('False')
---
+++ hw2/problem23_3.py
@@ -0,0 +1,117 @@
+"""
+Author:
+Xinyi Ma xim002@ucsd.edu
+Yijun Zhang yiz160@ucsd.edu
+"""
+
+import sys
+import fileinput
+from collections import deque
+
+#Problem23_1
+
+lineNum = 0
+input = sys.stdin.read()
+text = input.splitlines()
+row = len(text)
+column = len(text[0].split(','))
+lines = []
+for j in range(0,row):
+    li = [int(x.strip()) for x in text[j].split(',')]
+    lines.append(li)
+
+#vacumm location not valid
+if len(lines[row-1]) == 2:
+    if (int(lines[row-1][0]) not in range(0,row-1)) or (int(lines[row-1][1]) not in range(0,column)):
+        sys.exit('invalid input: last line')
+else:
+    sys.exit('invalid input: last line')
+
+for i in range(0,row-1):
+    # Initial state not valid
+    for k in range(0, column):
+        if lines[i][k] not in [0,1]:
+            sys.exit('invalid input')
+            
+
+# Whether the state is a goal state
+count = 0
+for i in range(0, row-1):
+    for j in range(0, column):
+        if (lines[i][j] is 0):
+            count += 1
+        else:
+            break
+    if count != column:
+        break
+# Check if they are all zeros
+if count == (row-1)*column:
+    sys.exit('')
+
+
+
+#Problem23_2 DFS
+frontier = [lines]
+print type(frontier)
+visited = [lines]
+path = {}
+path[str(tuple(lines))] = ''
+action = 'SDRUL'
+
+while len(frontier) != 0:
+    node = frontier.pop()
+    print 'this is node: '
+    print node
+    room = node[row-1]
+    print room
+
+    #check goal state
+    count = 0
+    for i in range(0, row-1):
+        for j in range(0, column):
+            if (lines[i][j] is 0):
+                count += 1
+            else:
+                break
+        if count != column:
+            break
+    # Check if they are all zeros
+    if count == (row-1)*column:
+        sys.exit('')
+
+
+    #run DFS
+    step=path[str(tuple(node))]
+
+    for act in action:
+        #print act
+        if act == 'S':
+            temp = list(node)
+            temp[room[0]][room[1]]=0
+            if temp in visited:
+                continue
+            else:
+                frontier.append(temp)
+                visited.append(temp)
+                path[str(tuple(temp))]=step+act
+
+        if act=='U':
+            temp=list(node)
+            temp[row-1][0]=room[0]-1
+                if temp in visited:
+                    continue
+            elif temp[row-1][0]>=0:
+                frontier.append(temp)
+                visited.append(temp)
+                path[tuple(temp)]=step+act
+
+        if act=='D':
+            temp=list(node)
+            temp[row-1][0]=room[0]+1
+            if temp in visited:
+                continue
+            elif temp[row-1][0]<=row-2:
+                frontier.append(temp)
+                visited.append(temp)
+                path[tuple(temp)]=step+act
+
