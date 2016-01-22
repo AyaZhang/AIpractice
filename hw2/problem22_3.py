c6fb026c0dbab7553827de2c61fb3ac0b2f37da7
mxy2008 <mxy2008@users.noreply.github.com>
2016-01-21 23:20:54 -0800

    Update problem22_3.py

--- hw2/problem22_3.py
+++ hw2/problem22_3.py
@@ -13,7 +13,7 @@ for line in fileinput.input():
 
     try:
         arrangement = [int(x.strip()) for x in line.split(',')]
-        length=len(arrangement)
+        length = len(arrangement)
 
     except:
         sys.exit('invalid input')
@@ -26,17 +26,17 @@ for line in fileinput.input():
     for k in range(0, length-1):
         if arrangement[k] not in [0, 1]:
             sys.exit('invalid input')
-    if arrangement[length-1]>=length-1:
+    if arrangement[length-1] >= length-1:
         sys.exit('invalid input')
 
     # determine whether the state is a goal state
     count=0
     for k in range(0, length-1):
         if arrangement[k] is 0:
-            count+=1
+            count += 1
         else:
             break
-    if count==length-1:
+    if count == length-1:
         sys.exit('')
 
 #Problem21_3  DFS
@@ -52,14 +52,14 @@ for line in fileinput.input():
         visited.append(node)
         
         #find goal state
-        count=0
+        count = 0
         for k in range(0, length-1):
             if node[k] is 0:
-                count+=1
+                count += 1
             else:
                 break
-        if count==length-1:
-            print(path[id(node)])
+        if count == length-1:
+            print path[id(node)]
             sys.exit()
 
         #run DFS
@@ -68,29 +68,29 @@ for line in fileinput.input():
         for act in action:
             if act == 'S':
                 temp = list(node)
-                temp[room]=0
+                temp[room] = 0
                 if temp in visited:
                     continue
                 else:
                     frontier.append(temp)
-                    path[id(temp)]=step+act
+                    path[id(temp)] = step+act
 
             elif act == 'R':
                 temp = list(node)
                 temp[length-1] = room+1
                 if temp in visited:
                     continue
-                elif temp[length-1]<=length-2:
+                elif temp[length-1] <= length-2:
                     frontier.append(temp)
-                    path[id(temp)]=step+act
+                    path[id(temp)] = step+act
 
             elif act == 'L':
                 temp = list(node)
                 temp[length-1] = room-1
                 if temp in visited:
                     continue
-                elif temp[length-1]>=0:
+                elif temp[length-1] >= 0:
                     frontier.append(temp)
-                    path[id(temp)]=step+act
+                    path[id(temp)] = step+act
     sys.exit('')
 
