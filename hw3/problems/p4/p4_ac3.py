546c2039e7f9ef1d4f90366547258fd15c18c1d2
Xinyi Ma <xinyima9@gmail.com>
2016-02-01 19:03:30 -0800

    improve

--- hw3/problems/p4/p4_ac3.py
+++ hw3/problems/p4/p4_ac3.py
@@ -16,20 +16,45 @@ def ac3(csp, arcs=None):#implement backtracking here?
 
     This method returns True if the arc consistency check succeeds, and False otherwise."""
 
-    queue_arcs = deque(arcs if arcs is not None else csp.constraints.arcs())
 
-    [xi, xj] = queue_arcs.popleft()
+    # TODO implement this
+    while queue_arcs is not empty:
+        [xi, xj] = queue_arcs.popleft()
+        if remove_inconsistent_values(xi,xj):
+            for [xi,xk] in csp.constraints[xi].arcs():  #csp.constraints[xi].arcs() find all constraint related to xi, and xk is the neighbor of xi(a problem here: xk sometime is the same as xi and some xk are repetitive)
+                queue_arcs.add(tuple(xk,xi))
+
+def remove_inconsistent_values(xi, xj):
+    removed = False
+    dom = xi.domain
+    for x in xi.domain:
+        xi.assign(x)
+        while y in xj.domain:
+            xj.assign[y]
+            if not csp.constraints[xi,xj]:
+                #xi.domain = [value in dom if value is not x]   #delete x from domain[xi]
+                removed = True
+    return removed
+
+def revise(csp, xi, xj):
+    # You may additionally want to implement the 'revise' method.
+    pass
+
+"""
+#debug here
+[xi, xj] = queue_arcs.popleft()
     print [xi, xj]
     print [xi]
     print csp.constraints[xi].arcs()
     list = deque(csp.constraints[xi].arcs())
     for [xi, xk] in csp.constraints[xi].arcs():
         print xk
-        #for [xi, xk] in list:
+#for [xi, xk] in list:
 #print xk
 #for constraint in csp.constraints[xi]:
 #       print constraint
 #print type(constraint)
+"""
 """
     print queue_arcs
     for item in queue_arcs:
@@ -37,28 +62,13 @@ def ac3(csp, arcs=None):#implement backtracking here?
     print type(item)
     print item[1]
     """
-
 """
-    # TODO implement this
-    while queue_arcs is not empty:
-        [xi, xj] = queue_arcs.pop()  #pop()?
-        if remove-inconsistent-values(xi,xj):
-            for xk in csp.constraints[xi]:  #neighbors?
-                queue_arcs.add(tuple(xk,xi))
-
-def remove_inconsistent-values(xi, xj)
-    removed = False
-    dom = xi.domain
-    for x in xi.domain:
-        xi.assign(x)
-        while y in xj.domain:
-            xj.assign[y]
-            if !csp.constraints[xi,xj]:
-                xi.domain = [value in dom if value is not x]   #delete x from domain[xi]
-                removed = True
-    return removed
-
-def revise(csp, xi, xj):
-    # You may additionally want to implement the 'revise' method.
-    pass
+    dom = [1,2,3,4]
+    x = 1
+    y = []
+    for value in dom and value is not x:
+    y.append(value)
+    print y
+    
+    queue_arcs = deque(arcs if arcs is not None else csp.constraints.arcs())
 """
